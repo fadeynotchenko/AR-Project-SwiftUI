226d70de9d7a82234e494bf4f39936ed2fcdae11
fadeynotchenko <notchenkofaddey@gmail.com>
2022-02-22 14:52:49 +0300

    Initial Commit

---
+++ AR Project SwiftUI/AR_Project_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_Project_SwiftUIApp.swift
+//  AR Project SwiftUI
+//
+//  Created by Fadey Notchenko on 22.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct AR_Project_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AR Project SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AR Project SwiftUI
+//
+//  Created by Fadey Notchenko on 22.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
